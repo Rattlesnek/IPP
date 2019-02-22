67351c0768d90d8a97ca3293303c7e998a254410
Adam Pankuch <adam.pankuch@gmail.com>
2019-02-22 14:33:47 +0100

    Parser complete

--- parse.php
+++ parse.php
@@ -1,8 +1,9 @@
 <?php
 include 'stats.php';
 
-const ERR_INF = 11;
-const ERR_OUTF = 12;
+const ERR_PARAM = 10;
+const ERR_INFILE = 11;
+const ERR_OUTFILE = 12;
 const ERR_HEAD = 21;
 const ERR_OPCODE = 22;
 const ERR_LEX_SYN = 23;
@@ -16,11 +17,9 @@ const instructions = array(
     'DEFVAR'        => 'var',
     'CALL'          => 'label',
     'RETURN'        => '',
-    
-    'PUSHS'         => 'symb',
+/**/'PUSHS'         => 'symb',
     'POPS'          => 'var',
-
-    'ADD'           => 'var symb symb',
+/**/'ADD'           => 'var symb symb',
     'SUB'           => 'var symb symb',
     'MUL'           => 'var symb symb',
     'IDIV'          => 'var symb symb',
@@ -32,60 +31,40 @@ const instructions = array(
     'NOT'           => 'var symb symb',
     'INT2CHAR'      => 'var symb',
     'STR2INT'       => 'var symb symb',
-    
-    'READ'          => 'var type',
+/**/'READ'          => 'var type',
     'WRITE'         => 'symb',
-
-    'CONCAT'        => 'var symb symb',
+/**/'CONCAT'        => 'var symb symb',
     'STRLEN'        => 'var symb',
     'GETCHAR'       => 'var symb symb',
     'SETCHAR'       => 'var symb symb',
-
-    'TYPE'          => 'var symb',
-
-    'LABEL'         => 'label',
+/**/'TYPE'          => 'var symb',
+/**/'LABEL'         => 'label',
     'JUMP'          => 'label',
     'JUMPIFEQ'      => 'label symb symb',
     'JUMPIFNEQ'     => 'label symb symb',
     'EXIT'          => 'symb',
-
-    'DPRINT'        => 'symb',
+/**/'DPRINT'        => 'symb',
     'BREAK'         => ''
 );
 
 const operands = array(
-    'var'   => array(0),
-    'symb'  => array(0, 1, 2, 3, 4),
-    'label' => array(5),
-    'type'  => array(6)
-);
-
-const map = array(
-    0 => 'var',
-    1 => 'nil',
-    2 => 'int',
-    3 => 'bool',
-    4 => 'string',
-    5 => 'label',
-    6 => 'type'
+    'var'   => [0],
+    'symb'  => [0, 1, 2, 3, 4],
+    'label' => [5],
+    'type'  => [6]
 );
 
 const regexes = array(
-    0 => '/^(GF|TF|LF)@([a-zA-Z_\-$&%*!?][a-zA-Z0-9_\-$&%*!?]*)$/',  # VAR
-    1 => '/^(nil)@(nil)$/', # NIL
-    2 => '/^(int)@(.+)$/',  # INT
-    3 => '/^(bool)@(true|false)$/', # BOOL 
-    4 => '/^(string)@((\\\\[0-9]{3}|((?!#|\\\\)(?!\p{Z})\P{C})*)*)$/u', # STRING
+    0 => '/^((GF|TF|LF)@[a-zA-Z_\-$&%*!?][a-zA-Z0-9_\-$&%*!?]*)$/',  # VAR
+    1 => '/^nil@(nil)$/', # NIL
+    2 => '/^int@(.+)$/',  # INT
+    3 => '/^bool@(true|false)$/', # BOOL 
+    4 => '/^string@((\\\\[0-9]{3}|((?!#|\\\\)(?!\p{Z})\P{C})+)+)$/u', # STRING
     5 => '/^([a-zA-Z_\-$&%*!?][a-zA-Z0-9_\-$&%*!?]*)$/', # LABEL
     6 => '/^(int|string|bool)$/' # TYPE
 );
 
-function map_text($matches, $reg_idx) {
-    if (1 <= $reg_idx and $reg_idx <= 4)
-        return $matches[2];
-    else
-        return $matches[0];
-}
+const map_type = array('var', 'nil', 'int', 'bool', 'string', 'label', 'type');
 
 function error($error_string, $ret_code) {
     fprintf(STDERR, $error_string);
@@ -93,7 +72,21 @@ function error($error_string, $ret_code) {
 }
 
 function help() {
-    echo "HELP\n";    
+    echo "HELP\n";   
+    exit(0); 
+}
+
+function handle_options($opt) {
+    if (array_key_exists('help', $opt) or array_key_exists('h', $opt)) {
+        help();
+    } elseif (array_key_exists('stats', $opt)) {
+        return true;
+    } elseif (array_key_exists('loc', $opt) or array_key_exists('comments', $opt) or
+              array_key_exists('labels', $opt) or array_key_exists('jumps', $opt)) {
+        error("ERROR: param --stats=file is missing!\n", ERR_PARAM);
+    } else {
+        return false;
+    }
 }
 
 function check_header($line) {
@@ -101,92 +94,78 @@ function check_header($line) {
         error("ERROR: wrong file header!\n", ERR_HEAD);
 }
 
-function addElement($dom, $to, $name) {
-    $elem = $dom->createElement($name);
-    $to->appendChild($elem);
-    return $elem;
-}
-
 const SHORT_OPTS = 'hs:lcbj';
 const LONG_OPTS = array('help', 'stats:', 'loc', 'comments', 'labels', 'jumps');
 $opt = getopt(SHORT_OPTS, LONG_OPTS);
-if (array_key_exists('help', $opt) or array_key_exists('h', $opt))
-    help();
+$enable_stats = handle_options($opt);
 
-var_dump($opt);
+$keys = array_keys($opt);
+if ($enable_stats) {
+    $stat_file = fopen($opt['stats'], 'w');
+    if (! $stat_file) {
+        error("ERROR: ".$e."\n", ERR_OUTFILE);
+    }
+    $keys = array_diff($keys, ['stats']);
+}
 
-$stats = new Stats();
+// create XML and Statistics
+$XML = new XMLCreator();
+$stats = new Stats($keys);
 
 $line = fgets(STDIN);
 check_header($line);
 
-# create XML
-$DOM = new DOMDocument('1.0', 'UTF-8');
-$DOM->formatOutput = true;
-$progXML = addElement($DOM, $DOM, 'program');
-$progXML->setAttribute('language', 'IPPcode19');
-
-# MAIN LOOP
-$ins_cnt = 0;
+// MAIN LOOP
 while ($line = fgets(STDIN)) {
-    echo "line: $line";
-    
-    $parts = preg_split('/\s+/', trim($line));
-    $instruct = strtoupper(array_shift($parts));
-    # if empty line then continue in main loop
-    if ($instruct == '') {
-        continue;
-    } elseif ($instruct[0] == '#') {
+    //echo "line: $line";    
+    $comment_parts = preg_split('/#/', trim($line));
+    if (count($comment_parts) > 1) {
         $stats->incComments();
-        continue;
-    } else {
-        $stats->incInstructs($instruct);
     }
+    $parts = preg_split('/\s+/', trim($comment_parts[0]));
+    $opcode = strtoupper(array_shift($parts));
 
-    echo " ... instruct: $instruct\n";
-    
-    if (array_key_exists($instruct, instructions)) {
-        $ins_cnt++;
-        $insXML = addElement($DOM, $progXML, 'instruction');
-        $insXML->setAttribute('order', $ins_cnt);
-        $insXML->setAttribute('opcode', $instruct);
-        
-        $inst_operands = explode(' ', instructions[$instruct]);
-        # for instructions that dont have operands - see behavior of explode()
-        if ($inst_operands[0] == '')
+    // if empty line then continue in main loop
+    if ($opcode == '' or $opcode[0] == '#') {
+        continue;
+    }
+    //echo " ... instruct: $opcode\n";
+    if (array_key_exists($opcode, instructions)) {
+        $stats->incInstructs($opcode);
+        $instructXML = $XML->addInstruction($opcode);
+
+        $inst_operands = explode(' ', instructions[$opcode]);
+        // for instructions that dont have operands - see behaviour of explode()
+        if ($inst_operands[0] == '') {
             array_shift($inst_operands);
-
-        if (count($inst_operands) != count($parts))
+        }
+        if (count($inst_operands) != count($parts)) {
             error("ERROR: wrong number of operands!\n", ERR_LEX_SYN);
-
+        }
         for ($i = 0; $i < count($inst_operands); $i++) {
-            echo " ... operand_$i: ", $inst_operands[$i], " vs \"", $parts[$i], "\"\n";
-            
-            $success = FALSE;
+            //echo " ... operand_$i: ", $inst_operands[$i], " vs \"", $parts[$i], "\"\n";
+            $success = false;
             foreach (operands[$inst_operands[$i]] as $reg_idx) {
                 if (preg_match(regexes[$reg_idx], $parts[$i], $matches)) {
-                    $success = TRUE;
+                    $success = true;
                     break;
                 }
             }
-            if ($success == FALSE)
+            if ($success == false) {
                 error("ERROR: regex failed!\n", ERR_LEX_SYN); 
-            
-            $argXML = $DOM->createElement('arg'.($i+1), map_text($matches, $reg_idx));
-            $argXML->setAttribute('type', map[$reg_idx]);
-            $insXML->appendChild($argXML);
+            }
+            $XML->addArgument($instructXML, $i+1, map_type[$reg_idx], $matches[1]);
         }
     } else {
         error("ERROR: wrong or unknown opcode!\n", ERR_OPCODE);
     }
 }
+//echo "====== SUCCESS ======\n\n";
 
-echo "====== SUCCESS ======\n\n";
+if ($enable_stats) {
+    fwrite($stat_file, $stats->str());
+    fclose($stat_file);
+}
 
-echo $DOM->saveXML();
-echo "\n";
-
-echo $stats->str();
-
-
+echo $XML->str();
 ?>
--- stats.php
+++ stats.php
@@ -1,12 +1,14 @@
 <?php
 
 class Stats {
+    private $select;
     private $loc;
     private $comments;
     private $labels;
     private $jumps;
 
-    public function __construct($loc=0, $comments=0, $labels=0, $jumps=0) {
+    public function __construct($select, $loc=0, $comments=0, $labels=0, $jumps=0) {
+        $this->select = $select;
         $this->loc = $loc;
         $this->comments = $comments;
         $this->labels = $labels;
@@ -22,15 +24,22 @@ class Stats {
 
         if ($instruct == 'LABEL')      
             $this->labels++;
-        elseif (strpos($instruct, 'JUMP') !== FALSE)
+        elseif (strpos($instruct, 'JUMP') !== false)
             $this->jumps++;
     }
 
     public function str() {
-        return  "loc:\t$this->loc\n" .
-                "comms:\t$this->comments\n" .
-                "labels:\t$this->labels\n" .
-                "jumps:\t$this->jumps\n";
+        $string = '';
+        if (in_array('loc', $this->select))
+            $string .= "$this->loc\n";
+        if (in_array('comments', $this->select))
+            $string .= "$this->comments\n";
+        if (in_array('labels', $this->select))
+            $string .= "$this->labels\n";
+        if (in_array('jumps', $this->select))
+            $string .= "$this->jumps\n";
+
+        return $string;
     }
 
     public function __get($attr) {
@@ -39,17 +48,45 @@ class Stats {
     }
 }
 
-class XMLcreator {
+function replace_amp($string) {
+    return str_replace('&', '&amp;', $string);
+}
+
+class XMLCreator {
     private $ins_cnt;
-    private $DOM;
+    public $DOM;
+    public $root;
 
     public function __construct() {
         $this->ins_cnt = 0;
+
         $this->DOM = new DOMDocument('1.0', 'UTF-8');
         $this->DOM->formatOutput = true;
-        $progXML = $this->DOM->createElement('program');
-        $progXML->setAttribute('language', 'IPPcode19');
-        $this->DOM->appendChild($progXML);
+
+        $this->root = $this->DOM->createElement('program');
+        $this->root->setAttribute('language', 'IPPcode19');
+        $this->DOM->appendChild($this->root);
+    }
+
+    public function addInstruction($opcode) {
+        $this->ins_cnt++;
+
+        $instructXML = $this->DOM->createElement('instruction');
+        $instructXML->setAttribute('order', $this->ins_cnt);
+        $instructXML->setAttribute('opcode', $opcode);
+        $this->root->appendChild($instructXML);
+        return $instructXML;
+    }
+
+    public function addArgument($instructXML, $number, $type, $value) {
+        $argXML = $this->DOM->createElement('arg'.$number, replace_amp($value));
+        $argXML->setAttribute('type', $type);
+        $instructXML->appendChild($argXML);
+        return $argXML;
+    }
+
+    public function str() {
+        return $this->DOM->saveXML();
     }
 }
 
